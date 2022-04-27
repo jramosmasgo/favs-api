074d412c40adcc01f4ebe10a78cc4568a9ca762a
Jean Ramos <jeanramos@Mac-mini-de-Jean.local>
2022-04-27 16:37:26 -0500

    fix: update swagger

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
+import open from "open";
 
 const databaseConnection = () => {
   mongoose
     .connect(`${process.env.CONNECTION_DATABASE}`)
-    .then((_res) => console.info("Database Connected"))
+    .then((_res) => {
+      console.info("Database Connected");
+      open("http://localhost:4000/docs");
+    })
     .catch((err) => console.error(err));
 };
 
